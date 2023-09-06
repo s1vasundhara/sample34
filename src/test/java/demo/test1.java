ffe7d887ee2933282cb5ae192f63cf82200f6b8f
admin <admin@vasundhara>
2023-09-06 09:14:58 +0530

    coomit

--- src/test/java/demo/test1.java
+++ src/test/java/demo/test1.java
@@ -1,5 +1,16 @@
 package demo;
 
+import org.testng.annotations.Test;
+
 public class test1 {
+	@Test
+	public void test2()
+	{
+		System.out.println("test2");
+	}
+	public void test3()
+	{
+		System.out.println("test3");
+	}
 
 }
